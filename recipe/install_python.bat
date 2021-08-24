9717af826b434935831a8c3df20bcb25e1de6b5d
Tom de Geus <tom@geus.me>
2021-08-24 16:50:35 +0200

    Should work now

--- recipe/install_python.bat
+++ recipe/install_python.bat
@@ -3,14 +3,10 @@
 
 
 echo import os > my_cmake_args.py
-echo >> my_cmake_args.py
 echo args = [] >> my_cmake_args.py
-echo >> my_cmake_args.py
 echo if 'CMAKE_ARGS' in os.environ: >> my_cmake_args.py
 echo     args = list(filter(None, os.environ['CMAKE_ARGS'].split(' '))) >> my_cmake_args.py
-echo >> my_cmake_args.py
 echo args = [arg for arg in args if len(arg.split('DCMAKE_INSTALL_PREFIX')) == 1] >> my_cmake_args.py
-echo >> my_cmake_args.py
 echo print(' '.join(args)) >> my_cmake_args.py
 
 
--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -4,14 +4,10 @@
 echo "CMAKE_ARGS = " ${CMAKE_ARGS}
 
 echo "import os
-
 args = []
-
 if 'CMAKE_ARGS' in os.environ:
     args = list(filter(None, os.environ['CMAKE_ARGS'].split(' ')))
-
 args = [arg for arg in args if len(arg.split('DCMAKE_INSTALL_PREFIX')) == 1]
-
 print(' '.join(args))
 " > my_cmake_args.py
 
