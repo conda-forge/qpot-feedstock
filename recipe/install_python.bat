bba2d3c7bd5369ffca12c337f3d92974d064d0e9
Tom de Geus <tom@geus.me>
2021-08-24 16:20:46 +0200

    Fixing accents

--- recipe/install_python.bat
+++ recipe/install_python.bat
@@ -1,3 +1,3 @@
 :: %PYTHON% setup.py install --build-type Release %CMAKE_ARGS%
 :: with scikit-build incompatible arguments stripped from it
-for /f "usebackq tokens=*" %%a in (`python cmakeargs_noinstall.py`) do %PYTHON% setup.py install --build-type Release %%a
+for /f "usebackq tokens=*" %%a in ('python cmakeargs_noinstall.py') do %PYTHON% setup.py install --build-type Release %%a
