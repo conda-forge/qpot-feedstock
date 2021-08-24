e9d46ea4381d16254bbb66dbc224ff78efb6b127
Tom de Geus <tom@geus.me>
2021-08-24 16:37:32 +0200

    New try

--- recipe/install_python.bat
+++ recipe/install_python.bat
@@ -1,4 +1,18 @@
 :: %PYTHON% setup.py install --build-type Release %CMAKE_ARGS%
 :: with scikit-build incompatible arguments stripped from it
-python cmakeargs_noinstall.py > mytemp.txt
+
+
+echo import os > my_cmake_args.py
+echo >> my_cmake_args.py
+echo args = [] >> my_cmake_args.py
+echo >> my_cmake_args.py
+echo if 'CMAKE_ARGS' in os.environ: >> my_cmake_args.py
+echo     args = list(filter(None, os.environ['CMAKE_ARGS'].split(' '))) >> my_cmake_args.py
+echo >> my_cmake_args.py
+echo args = [arg for arg in args if len(arg.split('DCMAKE_INSTALL_PREFIX')) == 1] >> my_cmake_args.py
+echo >> my_cmake_args.py
+echo print(' '.join(args)) >> my_cmake_args.py
+
+
+python my_cmake_args.py > mytemp.txt
 %PYTHON% setup.py install --build-type Release < mytemp.txt
--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,6 +1,24 @@
 # ${PYTHON} setup.py install --build-type Release ${CMAKE_ARGS}
 # with scikit-build incompatible arguments stripped from it
+
 echo "CMAKE_ARGS = " ${CMAKE_ARGS}
-python cmakeargs_noinstall.py
-echo $PYTHON setup.py install --build-type Release `python cmakeargs_noinstall.py`
-$PYTHON setup.py install --build-type Release `python cmakeargs_noinstall.py`
+
+echo "import os
+
+args = []
+
+if 'CMAKE_ARGS' in os.environ:
+    args = list(filter(None, os.environ['CMAKE_ARGS'].split(' ')))
+
+args = [arg for arg in args if len(arg.split('DCMAKE_INSTALL_PREFIX')) == 1]
+
+print(' '.join(args))
+" > my_cmake_args.py
+
+
+python my_cmake_args.py
+
+echo $PYTHON setup.py install --build-type Release `python my_cmake_args.py`
+
+
+$PYTHON setup.py install --build-type Release `python my_cmake_args.py`
