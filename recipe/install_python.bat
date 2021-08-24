86fa4e721a7e6635683c0d22d2ac9d51a5f64ee0
Tom de Geus <tom@geus.me>
2021-08-24 16:24:28 +0200

    Fixing windows

--- recipe/install_python.bat
+++ recipe/install_python.bat
@@ -1,3 +1,4 @@
 :: %PYTHON% setup.py install --build-type Release %CMAKE_ARGS%
 :: with scikit-build incompatible arguments stripped from it
-for /f "usebackq tokens=*" %%a in ('python cmakeargs_noinstall.py') do %PYTHON% setup.py install --build-type Release %%a
+python cmakeargs_noinstall.py > mytemp.txt
+%PYTHON% setup.py install --build-type Release < mytemp.txt
