459ebe04c2b9c79df165a72c27bf8e381840c2e9
Tom de Geus <tdegeus@users.noreply.github.com>
2021-08-24 17:47:33 +0200

    v0.9.8 (#33)
    * v0.9.8
    
    * MNT: Re-rendered with conda-build 3.21.4, conda-smithy 3.12, and conda-forge-pinning 2021.08.24.09.50.37
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/install_python.sh
+++ recipe/install_python.sh
@@ -1,1 +1,14 @@
-python -m pip install . -vv
+# ${PYTHON} setup.py install --build-type Release ${CMAKE_ARGS}
+# with scikit-build incompatible arguments stripped from it
+
+
+echo "import os
+args = []
+if 'CMAKE_ARGS' in os.environ:
+    args = list(filter(None, os.environ['CMAKE_ARGS'].split(' ')))
+args = [arg for arg in args if len(arg.split('DCMAKE_INSTALL_PREFIX')) == 1]
+print(' '.join(args))
+" > my_cmake_args.py
+
+
+$PYTHON setup.py install --build-type Release `python my_cmake_args.py`
